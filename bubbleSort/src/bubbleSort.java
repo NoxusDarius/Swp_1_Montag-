df4b2aca424a5de49501ba3aab06ef7122e200ad
NoxusDarius <rmp.theghost@gmail.com>
2020-12-02 10:48:34 +0100

    a

--- bubbleSort/src/bubbleSort.java
+++ bubbleSort/src/bubbleSort.java
@@ -3,23 +3,21 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class bubbleSort {
-
+    static Scanner scanner =new Scanner(System.in);
     static void bubbleSort(int[] arr) {
         int count = 0;
 
-        int temp = 0;
+        int temp ;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length - i; j++) {
 
                 if (arr[j] < arr[j - 1]) {
-                    //swap elements
+
                     temp = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = temp;
                     count++;
-
                 }
-
             }
         }
 
@@ -28,34 +26,41 @@ public class bubbleSort {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+    }
+    static int[] rdarray(int auswahlGrenze, int auswahlgröße){
+        int[] arrrd = new int[auswahlgröße];
+        Random rd = new Random();
+        for (int i = 0; i < arrrd.length; i++) {
+        arrrd[i] = rd.nextInt(auswahlGrenze);
+       // System.out.println(arrrd[i]);
+        }
+        return arrrd;
+    }
+    static int größen(){
+        int auswahlGrenze2;
+
+        System.out.println("Geben sie die Obergrenze ein:[von 0-?]:");
+        auswahlGrenze2 = scanner.nextInt();
+        return auswahlGrenze2;
+    }
+    static int größen2(){
+        int auswahlgröße2;
+
+        System.out.println("Geben Sie bitte die Maximale Größe ein: ");
+        auswahlgröße2 = scanner.nextInt();
+        return auswahlgröße2;
 
     }
-static int[] rdarray(int auswahlGrenze, int auswahlgröße){
-    int[] arrrd = new int[auswahlgröße];
-    Random rd = new Random(); // creating Random object
-    for (int i = 0; i < arrrd.length; i++) {
-        arrrd[i] = rd.nextInt(auswahlGrenze); // storing random integers in an array
-
-    }
-    return arrrd;
-}
 
     public static void main(String[] args) {
 
-        int auswahlGrenze;
-        int auswahlgröße;
-
-        Scanner scanner =new Scanner(System.in);
-
-        System.out.println("Geben Sie bitte die Maximale Größe ein: ");
-        auswahlgröße = scanner.nextInt();
-
-
-
-        System.out.println("Geben sie die Obergrenze ein:[von 0-?]:");
-        auswahlGrenze = scanner.nextInt();
-          int[] arr= rdarray(auswahlGrenze,auswahlgröße);
-            bubbleSort(arr);
+        int auswahlgröße = größen2();
+        int auswahlGrenze = größen();
+        final long timeStart = System.nanoTime();
+        int[] arr= rdarray(auswahlGrenze,auswahlgröße);
+        final long timeEnd = System.nanoTime();
+        System.out.println("Es wurden: " + (timeEnd-timeStart) + " Nanosekunden gemessen");
+        bubbleSort(arr);
 
 
         }
