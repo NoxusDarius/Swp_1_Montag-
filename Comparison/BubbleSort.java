4222c219f2f7b5642ee4a7dd32fdc4cadc103977
NoxusDarius <rmp.theghost@gmail.com>
2021-06-10 09:28:13 +0200

    14.07.2021

---
+++ Comparison/BubbleSort.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BubbleSort {
+    static public ArrayList<Long> bubblesortZeit = new ArrayList<>();
+
+
+    static int[] bubbleSort(int[] arr) {
+        int vertauschoperationen = 0;
+        int anzahlSchritte = 0;
+        int temp;
+
+        long startTime = (int) System.nanoTime();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i-1; j++) {
+                anzahlSchritte++;
+                if (arr[j] < arr[j - 1]) {
+
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                    vertauschoperationen++;
+                }
+            }
+        }
+
+        int endtime = (int) System.nanoTime();
+        int[] countWerte = new int[3];
+        countWerte[1] = vertauschoperationen;
+        countWerte[0] = anzahlSchritte;
+
+
+        bubblesortZeit.add(endtime - startTime);
+
+        return countWerte;
+
+
+    }
+
+}
---
+++ Comparison/InsertionSort.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class InsertionSort {
+
+    static public ArrayList<Long> bubblesortZeit = new ArrayList<>();
+
+    static int[] sort(int arr[]) {
+        int vertauschoperationen = 0;
+        int anzahlSchritte = 0;
+
+        int n = arr.length;
+        long startTime = (int) System.nanoTime();
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+
+                vertauschoperationen++;
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            anzahlSchritte++;
+            arr[j + 1] = key;
+        }
+        int endtime = (int) System.nanoTime();
+        int[] countWerte = new int[2];
+        countWerte[1] = vertauschoperationen;
+        countWerte[0] = anzahlSchritte;
+      //System.out.println("Hier die vergleichsoperationen"+ vertauschoperationen);
+
+        bubblesortZeit.add(endtime - startTime);
+        return countWerte;
+    }
+
+}
